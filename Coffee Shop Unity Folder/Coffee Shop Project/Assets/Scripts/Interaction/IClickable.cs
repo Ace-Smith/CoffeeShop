58b9af2887f998a15ee54512ceb87d84f454b730
Ace Smith <graceella0609@gmail.com>
2026-09-17 16:36:42 +0100

    feat: clickableObjects

---
+++ Coffee Shop Unity Folder/Coffee Shop Project/Assets/Scripts/Interaction/IClickable.cs
@@ -0,0 +1,14 @@
+//This is our inerface
+//It acts a socket between two bits of code
+
+//Think of it as a type c charger
+//It doesnt matter what it is plegged into, as long as:
+//It has a type c port
+
+//We can call  this
+public interface IClickable
+{
+    //and then it will look for this piece of code
+    //on the other code block
+    void OnClick();
+}
